fe57947220ca48ec0ca0221f8be9e091c5996471
jin <52230293+jin0731@users.noreply.github.com>
2021-10-03 16:16:45 +0900

    a

--- src/main/fetch_select.v
+++ src/main/fetch_select.v
@@ -1,7 +1,11 @@
+ /* a */
+
 module selector (in1, in2, sel, out);
 	input [3:0] in1, in2;
 	input sel;
 	output [3:0] out;
+
+
 	reg [3:0] out;
 		always @ (in1 or in2 or sel) begin
 			if(sel == 0)begin
